cff7b47585d55360c44a0a9779e6838de271fd07
Amrit Rau <amritrau@users.noreply.github.com>
2014-12-07 10:28:54 -0800

    Update Miller.cpp

--- Miller.cpp
+++ Miller.cpp
@@ -47,7 +47,7 @@ void loop(){
     	float poiLoc[3];
     	game.getPOILoc(poiLoc, targetPOI);
 	//This function is called once per second.  Use it to control the satellite.
-	if (game.getNextFlare() <15 && game.getNextFlare() !=-1)
+	if (game.getNextFlare() < 16 && game.getNextFlare() !=-1)
 	{
 	    api.setPositionTarget(darkzone);
         float attTarget[3];
@@ -69,7 +69,7 @@ void loop(){
                     
 
         game.getPOILoc(poiSpyLoc, targetPOI);
-        if (poiSpyLoc[2]>0.2)
+        if (poiSpyLoc[2]>0.15)
         {
             color = -1;
         }
@@ -155,7 +155,7 @@ void facePos(float target[3], float myPos[3]){
 		mathVecNormalize(attTarget,3);
 		api.setAttitudeTarget(attTarget);
 		
-        if (distanceVec(myPos, target)< 0.50)
+        if (distanceVec(myPos, target)< 1.50)
         {
             //DEBUG(("The SPHERE is close enough to take a picture. ")); //    
             if (game.alignLine(targetPOI)==true)
