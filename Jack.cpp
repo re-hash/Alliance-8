f37c64b652da4cde03417b9089ac03958211da25
Amrit Rau <amritrau@users.noreply.github.com>
2014-12-11 21:09:17 -0800

    Update Jack.cpp

--- Jack.cpp
+++ Jack.cpp
@@ -6,9 +6,12 @@ float darkzone[3];
 int picturesTaken;
 int color;
 int elapsed;
+int strategy;
+int flareCounter;
 
 void init(){
 	//This function is called once when your code is first loaded.
+	flareCounter = 0;
 	elapsed = 0;
     zero[0]=0.0;
 	zero[1]=0.0;
@@ -20,6 +23,7 @@ void init(){
 	darkzone[1]=0.0;
 	darkzone[2]=0.0;
 	picturesTaken = 0;
+	strategy =0;
 	ZRState myState;
 	api.getMyZRState(myState);
 	float myPos[3] = {myState[0], myState[1], myState[2]};
@@ -39,6 +43,8 @@ void init(){
 }
 
 void loop(){
+if (strategy==0)
+{    
 	ZRState myState;
     api.getMyZRState(myState);
     float myPos[3] = {myState[0], myState[1], myState[2]};
@@ -69,7 +75,7 @@ void loop(){
                     
 
         game.getPOILoc(poiSpyLoc, targetPOI);
-        if (poiSpyLoc[2]>0.15)
+        if (poiSpyLoc[2]>0.2)
         {
             color = -1;
         }
@@ -106,12 +112,19 @@ void loop(){
         }
 	else
 	{
-    	float target[3] = {0.0, 0.0, color*0.44};
-
-    	api.setPositionTarget(target);
-    	
-    	facePos(zero, myPos);
-        if (game.getMemoryFilled()==game.getMemorySize()) 
+    	    	if (game.getMemoryFilled()==0)
+    	{
+    	    float target[3] = {0.0, 0.0, color*0.44};
+    	    api.setPositionTarget(target);
+    	    facePos(zero, myPos);
+    	}
+    	else if (game.getMemoryFilled() == 1)
+    	{
+    	    float target[3] = {0.0, 0.0, color*0.38};
+    	    api.setPositionTarget(target);
+    	    facePos(zero, myPos);
+    	}
+        else if (game.getMemoryFilled()==game.getMemorySize())
         { //If SPHERE has a valid picture
             //DEBUG(("\n  Moving to upload"));
             //api.setPositionTarget(uploadTarget);
@@ -140,9 +153,65 @@ void loop(){
 
 	}
 	if (game.getFuelRemaining() <= 0)
+    {
+    game.turnOff();
+    //DEBUG(("\nGame over man, game over!"));
+    }
+    if (game.getNextFlare() == 29)
+    {
+        flareCounter++;
+    }
+    if (game.getScore() < game.getOtherScore() && (flareCounter/3)==2)
+    {
+        strategy = 1; 
+        DEBUG(("WHO NEEDS HUGGING WHEN YOU CAN PHOTOBOMB?????"))
+    }
+}
+else if (strategy==1)
 {
-game.turnOff();
-//DEBUG(("\nGame over man, game over!"));
+    stayInBounds();
+    ZRState myState;
+    ZRState otherState;
+    float myPos[3];
+    float otherPos[3];
+    api.getMyZRState(myState);
+    for (int i = 0; i<3; i++) {
+        myPos[i] = myState[i];
+    }
+    
+    if (game.getFuelRemaining() <= 30)
+    {
+        arcMove(darkzone);
+        //DEBUG(("\nGame over man, game over!"));
+    }
+    if (game.getFuelRemaining() <= 0)
+    {
+        game.turnOff();
+        //DEBUG(("\nGame over man, game over!"));
+    }
+    
+    else if (game.getNextFlare() <25 && game.getNextFlare() !=-1)
+	{
+	    arcMove(darkzone);
+	}
+    else {
+
+        api.getOtherZRState(otherState);
+        
+        for (int i = 0; i<3; i++) {
+            otherPos[i] = 1.2*otherState[i];
+        }
+        
+        if (distanceVec(myPos,otherPos) < 0.38) {
+            arcMove(otherPos);
+            DEBUG((" | Heading to otherPos | "));
+        }
+        else {
+            arcMove(otherState);
+            DEBUG((" | Heading to otherState | "));
+        }
+
+    }
 }
 elapsed++;	
 }
@@ -219,4 +288,54 @@ float distanceVec(float a[3], float b[3]) {  //finds distance between two object
 	float diff[3];
 	mathVecSubtract(diff,a,b,3);
 	return mathVecMagnitude(diff,3);
+}
+
+void stayInBounds() { 
+	float force[3] = {0.00001,0.00001,0.00001};
+	ZRState myState;
+	api.getMyZRState(myState);
+	//Tweak the numerical values as necessary based on the game boundaries.
+	if (myState[0] >=0.62 || myState[0] <=-0.62) {
+		force[0] = -myState[0];
+		DEBUG(("\n  Force X"));
+		
+	}
+	if (myState[1] >=0.76 || myState[1] <=-0.76) {
+			force[1] = -myState[1];
+				DEBUG(("\n  Force Y"));  
+	}
+	if (myState[2] >=0.62 || myState[2] <=-0.62) {
+			force[2] = -myState[2];
+					DEBUG(("\n  Force Z"));  
+	}
+	api.setForces(force);
+}
+
+void speedMove(float loc[3],float tarSpeed, ZRState myZRState) {
+		float velTar[3];
+		mathVecSubtract(velTar, loc, myZRState, 3);
+		float ratio = tarSpeed / mathVecMagnitude(velTar, 3);
+		
+		velTar[0] *= ratio;
+		velTar[1] *= ratio;
+		velTar[2] *= ratio;
+		api.setVelocityTarget(velTar);
+	}
+void arcMove(float posTarget2[3])
+{
+    ZRState myState;
+    api.getMyZRState(myState);
+    float myPos[3] = {myState[0], myState[1], myState[2]};
+    float midpoint[3] = {(myPos[0]+posTarget2[0])/2, (myPos[1]+posTarget2[1])/2, (myPos[2]+posTarget2[2])/2};
+    if (mathVecMagnitude(midpoint,3) < 0.29) {
+        mathVecNormalize(midpoint,3);
+     	for (int i = 0; i<3; i++) {
+    	 	midpoint[i] *= 0.48;
+     	}
+     	api.setPositionTarget(midpoint);
+     	DEBUG((" | Heading to waypoint | "));
+    }
+    else {
+        api.setPositionTarget(posTarget2);
+    }
 }
